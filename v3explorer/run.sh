2a240c8b79598d9ad427b956ac55296cae3c9451
Jae Park <jaejamespark@gmail.com>
2023-01-16 22:48:21 -0800

    add comment

--- v3explorer/run.sh
+++ v3explorer/run.sh
@@ -1,3 +1,6 @@
 pyenv local 3.9.16
 export PYTHONPATH="/home/jae/.pyenv/shims/python:/home/jae/git/hello_loopring:$PYTHONPATH"
 python3 api_explorer.py -a report
+
+# Not sure how to get account info to call API
+# https://www.reddit.com/r/loopringorg/comments/vyzir5/loopring_js_sdk/
